066a8a06b109b47bf51fa56c0963111416a4f384
Suyash Singh <suyashpratapts9@gmail.com>
2022-06-01 14:33:37 -0400

    Lavender Field base with optimized controls

---
+++ NoiseFields/js/lavenders/index.js
@@ -0,0 +1,301 @@
+import { EffectComposer } from 'https://unpkg.com/three@0.126.0/examples/jsm/postprocessing/EffectComposer.js';
+import { RenderPass } from 'https://unpkg.com/three@0.126.0/examples/jsm/postprocessing/RenderPass.js';
+import { AfterimagePass } from 'https://unpkg.com/three@0.126.0/examples/jsm/postprocessing/AfterimagePass.js';
+// Coolor constants (can be integrated)
+var gui;
+// Scene global variables
+const fpsLimit = 60;
+let clock = new THREE.Clock();
+let delta = 0;
+let interval = 1 / 60;
+var scene;
+var camera;
+var renderer;
+// Materials
+let composer;
+let afterimagePass;
+// Stats
+var stats = new Stats();
+// Particles, Noise and plant displayed
+var particles = [];
+
+const simplex = new SimplexNoise();
+// Render Constants
+var frameCount = 0;
+var noise = 0;
+var noiseOffset = Math.random() * 3;
+
+function setupGUI() {
+
+    gui = new lil.GUI;
+    var f1 = gui.addFolder('Flow Field');
+    var f2 = gui.addFolder('Particles');
+    var f3 = gui.addFolder('Colors');
+    var f3A = f3.addFolder('Particles');
+
+    f1.add(params, 'size', 1, 25).onFinishChange(resetSystem);
+    f1.add(params, 'noiseScale', 0, 0.1);
+    f1.add(params, 'noiseSpeed', 0, 0.025);
+    f1.add(params, 'noiseStrength', 0, 0.4);
+    f1.add(params, 'noiseFreeze');
+    //f2.add(params, 'particleCount', 0, 15000).onFinishChange(resetSystem);
+    f2.add(params, 'particleSize', 0, 1);
+    f2.add(params, 'particleSpeed', 0, 0.2);
+    f2.add(params, 'trailLen', 0.8, 1);
+    f3A.addColor(params, 'particleColor');
+    f3A.add(params, 'opacity', 0, 1);
+    f3A.addColor(params, 'particleColor2');
+    f3A.add(params, 'opacity2', 0, 1);
+    f3A.addColor(params, 'particleColor3');
+    f3A.add(params, 'opacity3', 0, 1);
+
+    var f3B = f3.addFolder('Background');
+    f3B.addColor(params, 'bgGradient1');
+    f3B.addColor(params, 'bgGradient2');
+    f3B.add(params, 'bgAngle', 0, 360, 1)
+
+    f1.close()
+        // f2.close()
+        // f3.close()
+        // gui.close()
+}
+
+
+
+function setupRenderer() {
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, .1, 2000);
+    camera.lookAt(0, 0, 0);
+    // camera.position.set(145, 130, 135);
+    camera.position.set(30, 50, 135);
+    camera.rotation.set(-0.08, 0.015, 0.05);
+
+    //renderer = new THREE.WebGLRenderer({ antiAlias: true, alpha: true, preserveDrawingBuffer: true });
+    renderer = new THREE.WebGLRenderer({ antiAlias: true, alpha: true });
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    //renderer.autoClearColor = false;
+    document.body.appendChild(renderer.domElement);
+
+    composer = new EffectComposer(renderer);
+    composer.addPass(new RenderPass(scene, camera));
+
+    afterimagePass = new AfterimagePass();
+    afterimagePass.uniforms['damp'].value = params.trailLen;
+    composer.addPass(afterimagePass);
+
+    stats.showPanel(0);
+    document.body.appendChild(stats.dom);
+}
+
+function updateBG() {
+    document.body.style.backgroundImage = "linear-gradient(" + params.bgAngle + "deg, " + params.bgGradient1 + ", " + params.bgGradient2 + ")";
+}
+
+function setupScene() {
+    const light = new THREE.PointLight(0xffffff, 1, 0);
+    light.position.set(50, 150, 0);
+
+    let octaGeo = new THREE.PlaneGeometry(1000, 100);
+    let octaMat = new THREE.MeshPhongMaterial({
+        color: 0x5b86e5,
+        side: THREE.DoubleSide,
+        opacity: 0.5,
+
+    });
+    let octa = new THREE.Mesh(octaGeo, octaMat)
+    octa.position.set(0, 0, -50)
+    octa.rotateX(Math.PI / 2)
+    scene.add(light);
+    //scene.add(octa);
+}
+
+function resize() {
+    let resolution = new THREE.Vector2(window.innerWidth, window.innerHeight);
+    let dpr = window.devicePixelRatio > 1 ? 2 : 1;
+
+
+    renderer.setPixelRatio(dpr);
+    renderer.setSize(resolution.x, resolution.y);
+    composer.setSize(resolution.x, resolution.y)
+
+    camera.aspect = resolution.x / resolution.y;
+    camera.updateProjectionMatrix();
+}
+
+function resetSystem() {
+    particles = [];
+    Wrule = GetAxiomTree();
+    pointGeometry = new THREE.Geometry()
+    pointGeometry.vertices.push(new THREE.Vector3(0, 0, 0));
+    scene.remove.apply(scene, scene.children);
+    frameCount = 0;
+    particlesInit(7, 0, 0, true)
+    particlesInit(35, 0, 0, true)
+    particlesInit(-7, 0, 0, false)
+    particlesInit(-35, 0, 0, false)
+
+    controls.setCustomState(new THREE.Vector3(1.13, 10.21, -3.65), new THREE.Vector3(1.5, 12.2, 24.7), 1)
+
+
+}
+
+
+function particlesInit(x, y, z, isRight) {
+    let dataPoints = giveMeField(x, y, z, 200, 0.18, isRight)
+        //let dataPoints = giveMeField(x, y, z, 30, 0.1)
+    for (let i = 0; i < dataPoints.length; i++) {
+        var p = new Particle(
+            Math.floor(dataPoints[i].x),
+            Math.floor(dataPoints[i].y),
+            Math.floor(dataPoints[i].z),
+            isRight
+        );
+        p.init(scene);
+        particles.push(p);
+        if (i == dataPoints.length) fitCameraToObject(camera, p.mesh, 2, controls)
+    }
+
+}
+
+
+function giveMeField(x, y, z, len, packDist, isRight) {
+    const fieldWidth = 8;
+    const xDelta = 0.12
+    let toReturn = []
+    const particleCount = len / packDist;
+    for (let i = 0; i < particleCount; i++) {
+        const xRandom = Math.random() * fieldWidth - (fieldWidth / 2)
+        let xOffset = 0
+        if (i > particleCount * 0.5) {
+            xOffset = i * boolToDirection(isRight) * -1 * xDelta / 40;
+        }
+        let zOffset = -1 * Math.pow(i, 0.91) * packDist;
+        toReturn.push(new THREE.Vector3(x + xOffset + xRandom, y, z + zOffset))
+    }
+    return toReturn;
+}
+
+
+function render() {
+    controls.update()
+    stats.begin();
+
+    // Update particles only with 60fps in mind
+    delta += clock.getDelta();
+    if (delta > interval) {
+
+        // Skip the slow start
+        if (frameCount < 60) {
+            while (frameCount < 60) {
+                frameCount++;
+                updateParticles();
+            }
+        }
+        updateParticles();
+        delta = delta % interval;
+    }
+    updateBG()
+    afterimagePass.uniforms['damp'].value = params.trailLen;
+    updateMaterial();
+    if (!params.noiseFreeze) frameCount++;
+    composer.render();
+    stats.end();
+    // setTimeout(function() {
+    // }, 1000 / 60);
+    requestAnimationFrame(render);
+}
+
+function updateParticles() {
+    for (var i = 0; i < particles.length; i++) {
+        var p = particles[i];
+        noise = simplex.noise3D(
+            p.pos.x * params.noiseScale,
+            p.pos.y * params.noiseScale,
+            p.pos.z * params.noiseScale + noiseOffset + frameCount * params.noiseSpeed
+        ) * Math.PI * 2;
+        p.angle.set(boolToDirection(p.isRight) * noise, noise, noise);
+        p.update();
+    }
+}
+
+function updateMaterial() {
+    material.color.setHex(params.particleColor);
+    mat2.color.setHex(params.particleColor2);
+    mat3.color.setHex(params.particleColor3);
+    material.size = params.particleSize;
+    mat2.size = params.particleSize;
+    mat3.size = params.particleSize;
+    material.opacity = params.opacity;
+    mat2.opacity = params.opacity2;
+    mat3.opacity = params.opacity3;
+}
+
+const fitCameraToObject = function(camera, object, offset, controls) {
+
+    offset = offset || 1.25;
+
+    const center = new THREE.Vector3(0, 0, 0);
+    const size = new THREE.Vector3(0, 0, 0);
+
+    const boundingBox = new THREE.Box3();
+
+    // get bounding box of object - this will be used to setup controls and camera
+    boundingBox.setFromObject(object);
+
+    boundingBox.getCenter(center);
+
+    boundingBox.getSize(size);
+
+    // get the max side of the bounding box (fits to width OR height as needed )
+    const maxDim = Math.max(size.x, size.y, size.z);
+    const fov = camera.fov * (Math.PI / 180);
+    let cameraZ = Math.abs(maxDim / 4 * Math.tan(fov * 2));
+
+    cameraZ *= offset; // zoom out a little so that objects don't fill the screen
+
+    camera.position.z = cameraZ;
+
+    const minZ = boundingBox.min.z;
+    const cameraToFarEdge = (minZ < 0) ? -minZ + cameraZ : cameraZ - minZ;
+
+    camera.far = cameraToFarEdge * 3;
+    camera.updateProjectionMatrix();
+
+    if (controls) {
+        // set camera to rotate around center of loaded object
+        controls.target = center;
+        // prevent camera from zooming out far enough to create far plane cutoff
+        controls.maxDistance = cameraToFarEdge * 2;
+        controls.saveState();
+        camera.lookAt(center)
+
+    } else {
+        camera.lookAt(center)
+    }
+}
+
+
+function setupOrbit() {
+    controls = new THREE.OrbitControls(camera, renderer.domElement);
+    //controls.enableDamping = true;
+    // controls.minPan = new THREE.Vector3(30, -35, 7);
+    // controls.maxPan = new THREE.Vector3(80, 65, 30)
+    // controls.minDistance = 1
+    // controls.maxDistance = 107
+    // controls.enableRotate = false
+    //controls.autoRotate = true;
+}
+
+
+setupGUI()
+setupRenderer();
+setupMaterials();
+setupOrbit();
+resize();
+window.addEventListener('resize', resize, false);
+resetSystem()
+setupScene();
+let theModule = { camera }
+window.myModule = theModule;
+render();
---
+++ NoiseFields/js/lavenders/particles.js
@@ -0,0 +1,191 @@
+var controls;
+var material;
+var plantMaterial;
+var mat2;
+var mat3;
+var pointGeometry = new THREE.Geometry();
+// Control variables
+var gui;
+let currentPreset = 0;
+// Positon of possible particles
+var dataPoints = [];
+var params = {
+    x: 0,
+    y: 90,
+    z: 0,
+    size: 9,
+    noiseScale: 0.014,
+    noiseSpeed: 0.003,
+    noiseStrength: 0.3,
+    noiseFreeze: false,
+    particleCount: 0,
+    particleSize: 0.41,
+    particleSpeed: 0.042,
+    particleColor: 0xee00ff,
+    opacity: 0.65,
+    particleColor2: 0x4400ff,
+    opacity2: 0.65,
+    particleColor3: 0xffffff,
+    opacity3: 0.65,
+    bgGradient1: "#36d1dc",
+    bgGradient2: "#5b86e5",
+    bgAngle: 165,
+    particleSkip: 2,
+    trailLen: 0.945,
+    animationDuration: 600,
+    Preset: 0,
+    renderIterations() {
+        particlesInit();
+    }
+};
+
+function setupMaterials() {
+    material = new THREE.PointsMaterial({
+        size: 1,
+        color: params.particleColor,
+        map: createCircleTexture('#ffffff', 256),
+        transparent: true,
+        depthWrite: false,
+        opacity: 0.60,
+        blending: THREE.AdditiveBlending
+    });
+
+    plantMaterial = new THREE.LineBasicMaterial({
+        color: params.particleColor,
+        transparent: true,
+        opacity: 0.0,
+        blending: THREE.AdditiveBlending
+    });
+
+    mat2 = new THREE.PointsMaterial({
+        size: 1,
+        map: createCircleTexture('#ffffff', 256),
+        transparent: true,
+        color: params.particleColor2,
+        depthWrite: false,
+        opacity: 0.55,
+        blending: THREE.AdditiveBlending
+    });
+
+    mat3 = new THREE.PointsMaterial({
+        size: 1,
+        map: createCircleTexture('#ffffff', 256),
+        transparent: true,
+        color: params.particleColor2,
+        depthWrite: false,
+        opacity: 0.55,
+        blending: THREE.AdditiveBlending
+    });
+}
+
+function createCircleTexture(color, size) {
+    var matCanvas = document.createElement('canvas');
+    matCanvas.width = matCanvas.height = size;
+    var matContext = matCanvas.getContext('2d');
+    // create texture object from canvas.
+    var texture = new THREE.Texture(matCanvas);
+    // Draw a circle
+    var center = size / 2;
+    matContext.beginPath();
+    matContext.arc(center, center, size / 2, 0, 2 * Math.PI, false);
+    matContext.closePath();
+    matContext.fillStyle = color;
+    matContext.fill();
+    // need to set needsUpdate
+    texture.needsUpdate = true;
+    // return a texture made from the canvas
+    return texture;
+}
+
+class Particle {
+    constructor(x, y, z, isRight) {
+        const zVariance = 20;
+        const zOffset = Math.floor((Math.random() * zVariance) - (zVariance / 4));
+        this.ex = x;
+        this.ey = y;
+        this.ez = z + zOffset;
+        this.shouldRun = Math.random() < 0.87;
+        if (!this.shouldRun) {
+            z = z + Math.floor(Math.random() * 8);
+            this.ez = z;
+        } else {
+            z += zOffset
+        }
+        this.pos = new THREE.Vector3(x, y, z);
+        this.vel = new THREE.Vector3(0, 0, 0);
+        this.acc = new THREE.Vector3(0, 0, 0);
+        this.angle = new THREE.Euler(0, 0, 0);
+        this.mesh = null;
+        //this.shouldChangeHue = Math.random() < 0.66;
+        this.randomBoundaryOffset = this.shouldRun ? (-params.size * 0.85) + (Math.random() * (params.size * 0.3)) : -params.size / 1.2;
+        //this.randomBoundaryOffset = -params.size / 1.2;
+        this.exceeded = false;
+        this.accelerated = false;
+        this.isRight = isRight
+    }
+
+    init(scene) {
+        let mat;
+        const choseMat = Math.floor(Math.random() * 3);
+        if (choseMat == 0) mat = material;
+        else if (choseMat == 1) mat = mat2;
+        else mat = mat3;
+
+        var point = new THREE.Points(pointGeometry, mat);
+        point.geometry.dynamic = true;
+        point.geometry.verticesNeedUpdate = true;
+        scene.add(point);
+        this.mesh = point;
+    }
+    update() {
+        if (this.isRight)
+            this.acc.set(1, 1.3, 0.1);
+        else
+            this.acc.set(-1, 1.3, 0.1);
+        this.acc.applyEuler(this.angle);
+        if (!this.shouldRun) {
+            this.acc.multiplyScalar(0.0007);
+        } else {
+            this.acc.multiplyScalar(params.noiseStrength);
+        }
+
+        this.acc.clampLength(0, params.particleSpeed);
+        this.vel.clampLength(0, params.particleSpeed);
+        this.vel.add(this.acc);
+
+        this.pos.add(this.vel);
+
+        // this.acc.multiplyScalar(params.particleDrag);
+        // this.vel.multiplyScalar(params.particleDrag);
+
+        //Position Resets
+
+        if (Math.abs(this.pos.x - this.ex) > params.size + this.randomBoundaryOffset || Math.abs(this.pos.y - this.ey) > params.size + this.randomBoundaryOffset ||
+            Math.abs(this.pos.z - this.ez) > params.size + this.randomBoundaryOffset) {
+            this.pos = new THREE.Vector3(this.ex, this.ey, this.ez)
+            if (!this.accelerated && this.shouldRun) {
+                this.randomBoundaryOffset = bottomHeavyRandom(params.size, 1.0);
+                this.accelerated = true;
+            }
+        }
+        this.mesh.position.set(this.pos.x, this.pos.y, this.pos.z);
+    }
+}
+
+
+// Bottom size is the percentage 
+function bottomHeavyRandom(limit, exceedBy) {
+    const isBottom = Math.random() > 0.8;
+    let toReturn = 0;
+    if (isBottom) {
+        toReturn = Math.random() * limit
+    } else {
+        toReturn = limit + Math.random() * (limit * exceedBy)
+    }
+    return toReturn;
+}
+
+function boolToDirection(trueOrFalse) {
+    if (trueOrFalse) return 1;
+    else return -1;
+}
