f4b4f678d88150fb420c4931c8f1d8fb3f67b389
Suyash Singh <suyash@my.yorku.ca>
2022-12-23 11:06:38 -0500

    rain unlimited

--- Lavender/js/lavenders/mqtt.js
+++ Lavender/js/lavenders/mqtt.js
@@ -36,8 +36,6 @@ const topicLimits = {
         max: 4096
     }
 }
-// Rain 
-// Invert lifeDivider
 const paramLimits = {
     "wind":{
         "strayParticleSpeed": {min: 0.01, max: 0.086},
@@ -48,11 +46,7 @@ const paramLimits = {
     "temp":{
         
         "particleMultiplier": {min: 1.5, max: 3}
-    },
-    // 650 is the maximum delta then, delta of 450 should change to 1.2, 2.5
-    // 
-    // Increase set size range such that value 350 on CO2 gives 1.2, but gives 2.5 on value 800 (multiplier will become even higher for value 1000)
-    // 
+    },    
     "co2":{
         "sizeMultiplier": {min: 1.2, max: 2.9},
         "noiseScale": {min: 0.006, max: 0.2},
@@ -62,16 +56,6 @@ const paramLimits = {
     "light":{
         "bgGradient2": {min: "0x473B68", max: "0xA082F2"}
     },
-    // Stray particles not going down to 0.04
-
-    //Rain 4095 or below, if there's any water on sensor, it's raining
-    //So, 4096 : No rain
-    //4095 slightly raining - 0 heavily pouring : Rain
-    // If humitiy is 90% or above, blue should start showing up: So humidity is like a button gatekeeping the rain sensor's interpolation
-    // IF humidity > 90 then particle color 2 : 0087fc (no interpolation)
-    // Don't change particle color 1 at all
-
-    // Stray particles 
     "rain":{
         "strayParticles": {min: 0.04, max: 0.1},
         // Particle color 2 :  - 0087fc
@@ -140,22 +124,18 @@ function liveUpdate(value, parameter, topicLimit, paramLimit){
         if(parameter == "strayParticles" || parameter=="particleMultiplier"){
             if(isInThreshold(params[parameter], scaledValue, paramLimit.max, paramLimit.min)){
                 if(scaleVal(value, topicLimit.min, topicLimit.max, 0, 1) < 0.13){
-                    if(parameter == "strayParticles"){
-                        if(currentHumidity>humidityThreshold){                 // Only change stray particles if humidity is above 90% (rain only works if there's humidity)
+                    if(parameter == "strayParticles"){                        
                             params[parameter] = paramLimit.min;
-                            resetSystem();
-                        }
+                            resetSystem();                        
                     }else {
                         params[parameter] = paramLimit.min;
                         resetSystem();
                     }
                     
                 }else{
-                    if(parameter == "strayParticles"){
-                        if(currentHumidity>humidityThreshold){                 // Only change stray particles if humidity is above 90% (rain only works if there's humidity)
+                    if(parameter == "strayParticles"){                        
                             params[parameter] = paramLimit.min;
-                            resetSystem();
-                        }
+                            resetSystem();                        
                     }else{
                         params[parameter] = scaledValue;
                         resetSystem();
